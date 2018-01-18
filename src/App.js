1c2d322b1b81f4b55dd4b61ca41ad44641a864be
Igor_Sysoev <Igor_Sysoev@epam.com>
2018-01-18 18:40:45 +0300

    add new rule first stage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,87 @@
 import React, {Component} from 'react';
+import {
+  Markup,
+  Editor,
+  Container,
+  Column,
+  Row,
+  RuleInput,
+  RuleLabel,
+  StyleInput,
+  Button,
+  Document
+} from './styled'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      editor: "",
+      name0: "",
+      begin0: "",
+      end0: "",
+      style0: "",
+      rules: 1
+    }
+  }
+
+
+  handleChange(e){
+    let {name, value} = e.target
+    console.log(value)
+    this.setState({
+      "editor": value
+    })
+  }
+
+  newFields() {
+    this.setState(prevState => {
+      let {rules} = prevState
+      let fields = ['name', 'begin', 'end', 'style']
+      let inputValues = {}
+      fields.forEach((field) => {
+        inputValues = {
+          ...inputValues,
+          [`${field}${rules}`]: ''
+        }
+      })
+      rules++
+      console.log({
+        rules, ...inputValues
+      })
+      return {
+        rules,
+        ...inputValues
+      }
+    })
+  }
+
   render() {
+    let {value} = this.state
+    let {handleChange, newFields} = this
     return (
-      <div>
-        <h2>Hello</h2>
-      </div>
-  )
-    ;
+      <Container>
+        <Column>
+          <Button onClick={this.newFields.bind(this)}>
+            New Rule
+          </Button>
+        </Column>
+        <Column>
+          <Button>
+            Random Text
+          </Button>
+          <Document>
+            <Editor
+              name={"Editor"}
+              value={value}
+              onChange={this.handleChange.bind(this)}
+            />
+            <Markup/>
+          </Document>
+        </Column>
+      </Container>
+    );
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
 				exclude: /(node_modules)/,
 				loader: "babel-loader",
 				query: {
-					presets: ["env", "react"]
+					presets: ["stage-0", "react"]
 				}
 			},
       {
